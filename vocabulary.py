ae49999f6966fcf350cc3fcc776dd130c020fa50
Bardin Petr <bardin.petr@gmail.com>
2018-05-14 14:28:18 +0300

    Changes in ABBYY Lingvo files

--- vocabulary.py
+++ vocabulary.py
@@ -1,16 +1,13 @@
 # coding: utf-8
 from __future__ import unicode_literals
+from libs.abbyy_lingvo import run
 
 
 def handle_dialog(request, response, user_storage):
     if request.is_new_session:
         response.set_text('Привет! Ты открыл словарь русского языка!')
         return response, user_storage
-
-    if request.command.lower() in ['ладно', 'куплю', 'покупаю', 'хорошо']:
-        response.set_text('Слона можно найти на Яндекс.Маркете!')
-        return response, user_storage
-
-    response.set_text('Все говорят "{}", а ты купи слона!'.format(request.command))
-
+    else:
+        txt = request.command.lower()
+        response.set_text('{} означает: {}'.format(txt, run(txt)))
     return response, user_storage
